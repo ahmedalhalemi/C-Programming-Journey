0dd191242eb3a5225eef93203c82be6d4649fff9
ahmedalhalemi <ahmdalhalemi@gmail.com>
2026-02-16 19:55:09 +0300

    Add welcome message and input validation

--- 01-basics/odd_even_checker.c
+++ 01-basics/odd_even_checker.c
@@ -1,16 +1,20 @@
 #include <stdio.h>
 
 int main() {
-    int number;
+    int input_number;
 
-    printf("Enter a number: ");
-    scanf("%d", &number);
+    printf("Welcome to the Odd/Even Checker Program!\n");
+    printf("Please enter a valid integer:\n");
 
-    if(number % 2 == 0) {
-        printf("%d is Even\n", number);
-    } else {
-        printf("%d is Odd\n", number);
+    if (scanf("%d", &input_number) != 1) {
+        printf("Error: Invalid input. Please enter a valid integer.\n");
+        return 1;
     }
 
+    if (input_number % 2 == 0)
+        printf("%d is Even.\n", input_number);
+    else
+        printf("%d is Odd.\n", input_number);
+
     return 0;
-}
+} 
