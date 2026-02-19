a500673a2f4a4ecf67fdae4a94f4d95c63c99a1d
ahmedalhalemi <ahmdalhalemi@gmail.com>
2026-02-19 10:17:19 +0300

    Add do-while loop to repeat program execution

--- 01-basics/odd_even_checker.c
+++ 01-basics/odd_even_checker.c
@@ -2,19 +2,29 @@
 
 int main() {
     int input_number;
+    char choice;
 
     printf("Welcome to the Odd/Even Checker Program!\n");
-    printf("Please enter a valid integer:\n");
 
-    if (scanf("%d", &input_number) != 1) {
-        printf("Error: Invalid input. Please enter a valid integer.\n");
-        return 1;
-    }
+    do {
+        printf("\nEnter a valid integer: ");
 
-    if (input_number % 2 == 0)
-        printf("%d is Even.\n", input_number);
-    else
-        printf("%d is Odd.\n", input_number);
+        if (scanf("%d", &input_number) != 1) {
+            printf("Invalid input. Please enter a valid integer.\n");
+            return 1;
+        }
+
+        if (input_number % 2 == 0)
+            printf("%d is Even.\n", input_number);
+        else
+            printf("%d is Odd.\n", input_number);
+
+        printf("Do you want to try again? (y = yes / n = no): ");
+        scanf(" %c", &choice);
+
+    } while (choice == 'y' || choice == 'Y');
+
+    printf("Program ended. Goodbye!\n");
 
     return 0;
-} 
+}
